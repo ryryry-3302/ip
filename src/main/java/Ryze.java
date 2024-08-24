9540143ae073756634def7e080cea900d775629c
ryryry-3302 <rjleong2002@gmail.com>
2024-08-24 20:51:55 +0800

    Feat: Add task method and add mark/unmark

--- src/main/java/Ryze.java
+++ src/main/java/Ryze.java
@@ -14,7 +14,7 @@ public class Ryze {
     }
 
     public static void main(String[] args) {
-        String[] listOfChatHistory = new String[100];
+        Task[] listOfChatHistory = new Task[100];
         int histoyIndex = 0;
 
         printDivider();
@@ -26,21 +26,50 @@ public class Ryze {
         String line = in.nextLine();
 
         while (!line.equals("bye")){
-            switch (line) {
+            String command = line.split(" ")[0];
+            int taskNumber = -1;
+            switch (command) {
             case "list":
                 printDivider();
                 for (int i = 0; i < listOfChatHistory.length; i++) {
                     if (listOfChatHistory[i] == null) {
                         break;
                     }
-                    System.out.printf("%d. %s\n", i + 1, listOfChatHistory[i]);
+                    System.out.printf("%d.[%s] %s\n", i + 1, listOfChatHistory[i].getStatusIcon(), listOfChatHistory[i].getDescription());
                 }
                 System.out.println();
                 printDivider();
                 break;
+            case "mark":
+            case "unmark":
+                try {
+                    taskNumber = Integer.parseInt(line.split(" ")[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid task number format.");
+                    break;
+                }
+
+                if (taskNumber > 0 && taskNumber <= histoyIndex) {
+                    if (command.equals("mark")) {
+                        listOfChatHistory[taskNumber - 1].markAsDone();
+                        printDivider();
+                        System.out.println("Nice! I've marked this task as done:");
+                    } else {
+                        listOfChatHistory[taskNumber - 1].markAsNotDone();
+                        printDivider();
+                        System.out.println("Okay! I've marked this task as not done:");
+                    }
+                    System.out.printf("  [%s] %s\n\n", listOfChatHistory[taskNumber - 1].getStatusIcon(), listOfChatHistory[taskNumber - 1].getDescription());
+                    printDivider();
+                } else {
+                    printDivider();
+                    System.out.println("Task number is out of range.\n");
+                    printDivider();
+                }
+                break;
             default:
                 echo("added: " + line);
-                listOfChatHistory[histoyIndex] = line;
+                listOfChatHistory[histoyIndex] = new Task(line);
                 histoyIndex++;
                 break;
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,4 +19,8 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    public String getDescription() {
+        return description;
+    }
+
 }
