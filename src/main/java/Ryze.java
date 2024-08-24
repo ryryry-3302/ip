0ab80637455a30a1a9d8356e881c80d3942b4321
ryryry-3302 <rjleong2002@gmail.com>
2024-08-24 19:31:27 +0800

    Feat: Add Echo

--- src/main/java/Ryze.java
+++ src/main/java/Ryze.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
 public class Ryze {
+    public static void printDivider(){
+        System.out.println("➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤➤");
+    }
+
+    public static void echo(String userInput){
+        System.out.println();
+        printDivider();
+        System.out.println(userInput);
+        System.out.println();
+        printDivider();
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        printDivider();
         System.out.println("Hello! I'm Ryze!");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        printDivider();
+        System.out.println();
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")){
+            echo(line);
+
+            line = in.nextLine();
+        }
+
+
+        System.out.println();
+        printDivider();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        printDivider();
     }
 }
